02580c8494bd44d8c2c9f58e937c5bc8e82edbb0
Lana Monson <lanamonson.slc@gmail.com>
2019-04-05 15:18:45 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+(function() {
+
+  //INITIALIZE FIREBASE
+  //create a project in firebase
+  //copy all the info below from project
+  var config = {
+    apiKey: "AIzaSyA9NFlOGDn5k3K8t8yKHQjBhLCW4k88LbY",
+    authDomain: "authentic2703.firebaseapp.com",
+    databaseURL: "https://authentic2703.firebaseio.com",
+    projectId: "authentic2703",
+    storageBucket: "authentic2703.appspot.com",
+    messagingSenderId: "752085827467"
+  };
+  firebase.initializeApp(config);
+
+
+  //GET ELEMENTS
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
+  const btnSignUp = document.getElementById('btnSignUp');
+  const btnLogout = document.getElementById('btnLogout');
+
+  //ADD LOGIN EVENT
+  bthLogin.addEventistener('click', e => {
+      //GET EMAIL AND PASS
+      const email = txtEmail.value;
+      const pass = txtPassword.value;
+      const auth = firebase.auth();
+      //SIGN IN
+      const promise = auth.signInWithEmailAndPassword(email. pass);
+      promise.catch(e => console.log(e.message));
+  });
+
+} ());
